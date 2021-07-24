4c7f5ac3ae7d3e5fa3b5eaf5b342bfb3c1b28218
Alireza S. Ghodsipoor <salirezag@gmail.com>
2021-07-24 10:22:44 +0430

    Display: reduce animate interval

--- Display.h
+++ Display.h
@@ -26,7 +26,7 @@ private:
         LINE_WIDTH = 1,
         SAMPLES = 400,
         MAX_Y_VALUE = 200,
-        UPDATE_INTERVAL_MS = 100
+        UPDATE_INTERVAL_MS = 50
     };
 
     QVector<int> xs, ys;
