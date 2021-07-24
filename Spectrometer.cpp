5e582b35444eca7b53ea248b5f6403575a40b982
Alireza S. Ghodsipoor <salirezag@gmail.com>
2021-07-24 11:54:07 +0430

    Rename m_*Button

--- Spectrometer.cpp
+++ Spectrometer.cpp
@@ -10,13 +10,13 @@ Spectrometer::Spectrometer(QWidget *parent)
     setWindowTitle("Spectrometer");
     setFixedSize(WIDTH, HEIGHT);
 
-    startButton->move(600, 20);
-    stopButton->move(600, 60);
+    m_startButton->move(600, 20);
+    m_stopButton->move(600, 60);
 
-    connect(startButton, &QPushButton::pressed, this, [this](){
+    connect(m_startButton, &QPushButton::pressed, this, [this](){
         m_display->start();
     });
-    connect(stopButton, &QPushButton::pressed, this, [this](){
+    connect(m_stopButton, &QPushButton::pressed, this, [this](){
         m_display->stop(); }
     );
 }
